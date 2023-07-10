11811af17f117cc9a8a4da52aa997bd0d5d09a9b
MatiasTixeira <matiastixeira.3240@gmail.com>
2023-07-10 19:48:22 -0300

    cambio en App.js, agregado de Partida.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,20 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {partida} from './components/partida';
+import {PPTLSCointext} from './context/partida/PPTLSContext';
 import React from 'react';
-import { useState } from 'react';
+
 
 function App() {
 
-  const [opcionDelJugador, setoOpcionJugador] = useState(0);
-  const [opcionDeLaMaquina, setoOpcionMaquina] = useState(0);
-  
-  const generarUnNumeroAleatorio = (minimo, maximo) => {
-    var numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );
-    return numero;
-  }
-
-  const jugar = () => {
-    setoOpcionMaquina(generarUnNumeroAleatorio(0,4))
-    console.log('Opcion del jugador')
-    console.log(opcionDelJugador)
-    console.log('Opcion de la maquina')
-    console.log(opcionDeLaMaquina)
-  }
-
   return (
-    <div>
-      <div>
-        <img src={require('../src/Assets/logo.jpg')} alt="Mi imagen" className='imagenPrincipal' />
-      </div>
-      <div>
-        <button onClick={() => setoOpcionJugador(0)} className='botonJugador'>Piedra</button>
-        <button onClick={() => setoOpcionJugador(1)} className='botonJugador'>Papel</button>
-        <button onClick={() => setoOpcionJugador(2)} className='botonJugador'>tijera</button>
-        <button onClick={() => setoOpcionJugador(3)} className='botonJugador'>Lagarto</button>
-        <button onClick={() => setoOpcionJugador(4)} className='botonJugador'>Spock</button>
-        <button onClick={jugar} className='botonJugar'>Jugar!</button>
-      </div>
-    </div>
+    <Router>
+      <PPTLSCointext>
+        <Switch>
+          <Route path="/home" Component={partida}/>
+        </Switch>
+      </PPTLSCointext>
+    </Router>
   );
 }
-
-// <img src={require('../src/Assets/ganador.png')} alt="Mi imagen" className='imagenGanador' />
 
 export default App;
